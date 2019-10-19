450707c7dc6ae53dc5af1a60065dfad6b0cfea31
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-10-19 13:50:41 +0200

    Remove isUniqueB from 1.1

--- 1/1.1.test.js
+++ 1/1.1.test.js
@@ -1,15 +1,8 @@
-import { isUniqueA, isUniqueB } from './1.1'
+import { isUniqueA } from './1.1'
 
 test('A', () => {
   expect(isUniqueA('abb')).toBe(false)
   expect(isUniqueA('abcb')).toBe(false)
   expect(isUniqueA('abc')).toBe(true)
   expect(isUniqueA('')).toBe(true)
-})
-
-test('B', () => {
-  expect(isUniqueB('abb')).toBe(false)
-  expect(isUniqueB('abcb')).toBe(false)
-  expect(isUniqueB('abc')).toBe(true)
-  expect(isUniqueB('')).toBe(true)
 })
