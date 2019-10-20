36bc2f30b9bd49630488a8b2b1d58e2176971bff
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-10-20 11:04:16 +0200

    Improve 1.4

--- 1/1.4.js
+++ 1/1.4.js
@@ -2,31 +2,25 @@ function isEven(num) {
   return num % 2 === 0
 }
 
+// Assumptions:
+// - case sensitive
+// - ignore spaces
+// - Unicode
 export function isPalindromePermutation(str) {
-  const len = str.length
-  let spaces = 0
   const hashMap = {}
-  for (let i = 0; i < len; i++) {
+  for (let i = 0; i < str.length; i++) {
     const char = str[i].toLowerCase()
-    if (char === ' ') spaces++
-    else {
+    if (char !== ' ') {
       if (hashMap[char]) hashMap[char]++
       else hashMap[char] = 1
     }
   }
 
   const values = Object.values(hashMap)
-  const lenNoSpaces = len - spaces
-  if (isEven(lenNoSpaces)) {
-    for (let i = 0; i < values.length; i++) {
-      if (!isEven(values[i])) return false
-    }
-  } else {
-    let oddCount = 0
-    for (let i = 0; i < values.length; i++) {
-      if (!isEven(values[i])) oddCount++
-      if (oddCount > 1) return false
-    }
+  let oddCount = 0
+  for (let i = 0; i < values.length; i++) {
+    if (!isEven(values[i])) oddCount++
+    if (oddCount > 1) return false
   }
 
   return true
