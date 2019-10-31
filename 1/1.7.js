586ee442e20d32a4296e080907807242c0ef7d54
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-10-31 16:05:55 +0100

    Update 1.7

--- 1/1.7.js
+++ 1/1.7.js
@@ -5,14 +5,14 @@ export function rotateImage(matrix) {
   const len = matrix.length
   for (let layer = 0; layer < Math.trunc(len / 2); layer++) {
     for (let i = layer; i < len - 1 - layer; i++) {
-      const top = matrix[layer][i + layer]
-      const right = matrix[i + layer][len - 1 - layer]
-      const bottom = matrix[len - 1 - layer][len - 1 - i - layer]
-      const left = matrix[len - 1 - i - layer][layer]
-      matrix[layer][i + layer] = left // set top
-      matrix[i + layer][len - 1 - layer] = top // set right
-      matrix[len - 1 - layer][len - 1 - i - layer] = right // set bottom
-      matrix[len - 1 - i - layer][layer] = bottom // set left
+      const top = matrix[layer][i]
+      const right = matrix[i][len - 1 - layer]
+      const bottom = matrix[len - 1 - layer][len - 1 - i]
+      const left = matrix[len - 1 - i][layer]
+      matrix[layer][i] = left // set top
+      matrix[i][len - 1 - layer] = top // set right
+      matrix[len - 1 - layer][len - 1 - i] = right // set bottom
+      matrix[len - 1 - i][layer] = bottom // set left
     }
   }
   return matrix
