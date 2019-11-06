926a191bb8d4e94224b37fe0095ee0658c14657a
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-11-06 16:51:53 +0100

    Handle empty list and increase test coverage in 2.1

--- 2/2.1.test.js
+++ 2/2.1.test.js
@@ -1,7 +1,7 @@
 import { removeDups } from './2.1'
 import LinkedList, { Element } from '../data-structures/LinkedList'
 
-test('test', () => {
+test('duplicates', () => {
   const numbers = new LinkedList(new Element(1))
   numbers.append(new Element(7))
   numbers.append(new Element(3))
@@ -11,6 +11,51 @@ test('test', () => {
   const expected = new LinkedList(new Element(1))
   expected.append(new Element(7))
   expected.append(new Element(3))
+  expected.append(new Element(5))
+
+  expect(removeDups(numbers)).toEqual(expected)
+})
+
+test('empty', () => {
+  const numbers = new LinkedList()
+
+  const expected = new LinkedList()
+
+  expect(removeDups(numbers)).toEqual(expected)
+})
+
+test('one element', () => {
+  const numbers = new LinkedList(new Element(1))
+
+  const expected = new LinkedList(new Element(1))
+
+  expect(removeDups(numbers)).toEqual(expected)
+})
+
+test('no duplicates', () => {
+  const numbers = new LinkedList(new Element(1))
+  numbers.append(new Element(3))
+  numbers.append(new Element(7))
+  numbers.append(new Element(5))
+
+  const expected = new LinkedList(new Element(1))
+  expected.append(new Element(3))
+  expected.append(new Element(7))
+  expected.append(new Element(5))
+
+  expect(removeDups(numbers)).toEqual(expected)
+})
+
+test('multiple duplicates', () => {
+  const numbers = new LinkedList(new Element(1))
+  numbers.append(new Element(3))
+  numbers.append(new Element(7))
+  numbers.append(new Element(5))
+  numbers.append(new Element(3))
+
+  const expected = new LinkedList(new Element(1))
+  expected.append(new Element(3))
+  expected.append(new Element(7))
   expected.append(new Element(5))
 
   expect(removeDups(numbers)).toEqual(expected)
