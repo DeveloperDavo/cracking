2c57fce35356016213e0c85313456c0f4e57b29c
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-12-15 16:42:10 +0100

    Add partition in order to 2.4

--- 2/2.4.js
+++ 2/2.4.js
@@ -1,5 +1,6 @@
 // Assumptions
 // - arguments have correct data type
+
 export function partition(head, x) {
   if (!head) {
     return head
@@ -19,4 +20,41 @@ export function partition(head, x) {
   }
 
   return head
+}
+
+export function partitionInOrder(head, x) {
+  if (!head) return head
+
+  let lowerHead,
+    lowerTail,
+    restHead,
+    restTail = null
+  let cur = head
+  while (cur !== null) {
+    const next = cur.next
+    cur.next = null
+    if (cur.value < x) {
+      if (!lowerHead) {
+        lowerHead = cur
+      } else {
+        lowerTail.next = cur
+      }
+      lowerTail = cur
+    } else {
+      if (!restHead) {
+        restHead = cur
+      } else {
+        restTail.next = cur
+      }
+      restTail = cur
+    }
+    cur = next
+  }
+
+  if (!lowerTail) {
+    return head
+  }
+
+  lowerTail.next = restHead
+  return lowerHead
 }
--- 2/2.4.test.js
+++ 2/2.4.test.js
@@ -1,149 +1,204 @@
-import { partition } from './2.4'
+import { partition, partitionInOrder } from './2.4'
 import { Element } from '../data-structures/LinkedList'
 
-test('test', () => {
-  const head = new Element(1)
-  head.next = new Element(5)
-  head.next.next = new Element(3)
-  head.next.next.next = new Element(4)
-  head.next.next.next.next = new Element(2)
+describe('partition', () => {
+  test('test', () => {
+    const head = new Element(1)
+    head.next = new Element(5)
+    head.next.next = new Element(3)
+    head.next.next.next = new Element(4)
+    head.next.next.next.next = new Element(2)
 
-  const x = 3
-  const newHead = partition(head, x)
+    const x = 3
+    const newHead = partition(head, x)
 
-  expect(newHead.value).toBeLessThan(x)
-  expect(newHead.next.value).toBeLessThan(x)
+    expect(newHead.value).toBeLessThan(x)
+    expect(newHead.next.value).toBeLessThan(x)
+  })
+
+  test('test different order', () => {
+    const head = new Element(4)
+    head.next = new Element(5)
+    head.next.next = new Element(3)
+    head.next.next.next = new Element(1)
+    head.next.next.next.next = new Element(2)
+
+    const x = 3
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeLessThan(x)
+    expect(newHead.next.value).toBeLessThan(x)
+  })
+
+  test('test another order', () => {
+    const head = new Element(3)
+    head.next = new Element(5)
+    head.next.next = new Element(4)
+    head.next.next.next = new Element(1)
+    head.next.next.next.next = new Element(2)
+
+    const x = 3
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeLessThan(x)
+    expect(newHead.next.value).toBeLessThan(x)
+  })
+
+  test('test all less than x', () => {
+    const head = new Element(3)
+    head.next = new Element(5)
+    head.next.next = new Element(4)
+    head.next.next.next = new Element(1)
+    head.next.next.next.next = new Element(2)
+
+    const x = 10
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeLessThan(x)
+    expect(newHead.next.value).toBeLessThan(x)
+    expect(newHead.next.next.next.next.value).toBeLessThan(x)
+  })
+
+  test('test all greater than x', () => {
+    const head = new Element(3)
+    head.next = new Element(5)
+    head.next.next = new Element(4)
+    head.next.next.next = new Element(1)
+    head.next.next.next.next = new Element(2)
+
+    const x = 0
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeGreaterThan(x)
+  })
+
+  test('test without x', () => {
+    const head = new Element(4)
+    head.next = new Element(5)
+    head.next.next = new Element(1)
+    head.next.next.next = new Element(2)
+
+    const x = 3
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeLessThan(x)
+    expect(newHead.next.value).toBeLessThan(x)
+  })
+
+  test('test other values', () => {
+    const head = new Element(1)
+    head.next = new Element(4)
+    head.next.next = new Element(3)
+    head.next.next.next = new Element(2)
+    head.next.next.next.next = new Element(5)
+    head.next.next.next.next = new Element(2)
+
+    const x = 3
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeLessThan(x)
+    expect(newHead.next.value).toBeLessThan(x)
+    expect(newHead.next.next.value).toBeLessThan(x)
+  })
+
+  test('test head greater than x', () => {
+    const head = new Element(6)
+    head.next = new Element(5)
+    head.next.next = new Element(3)
+    head.next.next.next = new Element(4)
+    head.next.next.next.next = new Element(2)
+
+    const x = 3
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeLessThan(x)
+  })
+
+  test('test head less than x', () => {
+    const head = new Element(1)
+    head.next = new Element(5)
+    head.next.next = new Element(3)
+    head.next.next.next = new Element(4)
+    head.next.next.next.next = new Element(2)
+
+    const x = 3
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeLessThan(x)
+    expect(newHead.next.value).toBeLessThan(x)
+  })
+
+  test('test partition in head', () => {
+    const head = new Element(3)
+    head.next = new Element(5)
+    head.next.next = new Element(1)
+    head.next.next.next = new Element(4)
+    head.next.next.next.next = new Element(2)
+
+    const x = 3
+    const newHead = partition(head, x)
+
+    expect(newHead.value).toBeLessThan(x)
+    expect(newHead.next.value).toBeLessThan(x)
+  })
+
+  test('empty list', () => {
+    const x = 3
+    const newHead = partition(null, x)
+
+    expect(newHead).toBe(null)
+  })
 })
 
-test('test different order', () => {
-  const head = new Element(4)
-  head.next = new Element(5)
-  head.next.next = new Element(3)
-  head.next.next.next = new Element(1)
-  head.next.next.next.next = new Element(2)
+describe('partition in order', () => {
+  test('test', () => {
+    const head = new Element(1)
+    head.next = new Element(5)
+    head.next.next = new Element(3)
+    head.next.next.next = new Element(4)
+    head.next.next.next.next = new Element(2)
 
-  const x = 3
-  const newHead = partition(head, x)
+    const x = 3
+    const newHead = partitionInOrder(head, x)
 
-  expect(newHead.value).toBeLessThan(x)
-  expect(newHead.next.value).toBeLessThan(x)
-})
+    const expected = new Element(1)
+    expected.next = new Element(2)
+    expected.next.next = new Element(5)
+    expected.next.next.next = new Element(3)
+    expected.next.next.next.next = new Element(4)
 
-test('test another order', () => {
-  const head = new Element(3)
-  head.next = new Element(5)
-  head.next.next = new Element(4)
-  head.next.next.next = new Element(1)
-  head.next.next.next.next = new Element(2)
+    expect(newHead).toEqual(expected)
+  })
 
-  const x = 3
-  const newHead = partition(head, x)
+  test('test head greater than x', () => {
+    const head = new Element(6)
+    head.next = new Element(5)
+    head.next.next = new Element(3)
+    head.next.next.next = new Element(4)
+    head.next.next.next.next = new Element(2)
 
-  expect(newHead.value).toBeLessThan(x)
-  expect(newHead.next.value).toBeLessThan(x)
-})
+    const x = 3
+    const newHead = partitionInOrder(head, x)
 
-test('test all less than x', () => {
-  const head = new Element(3)
-  head.next = new Element(5)
-  head.next.next = new Element(4)
-  head.next.next.next = new Element(1)
-  head.next.next.next.next = new Element(2)
+    const expected = new Element(2)
+    expected.next = new Element(6)
+    expected.next.next = new Element(5)
+    expected.next.next.next = new Element(3)
+    expected.next.next.next.next = new Element(4)
 
-  const x = 10
-  const newHead = partition(head, x)
+    expect(newHead).toEqual(expected)
+  })
 
-  expect(newHead.value).toBeLessThan(x)
-  expect(newHead.next.value).toBeLessThan(x)
-  expect(newHead.next.next.next.next.value).toBeLessThan(x)
-})
+  test('empty list', () => {
+    const x = 3
+    const newHead = partitionInOrder(null, x)
+    expect(newHead).toEqual(null)
+  })
 
-test('test all greater than x', () => {
-  const head = new Element(3)
-  head.next = new Element(5)
-  head.next.next = new Element(4)
-  head.next.next.next = new Element(1)
-  head.next.next.next.next = new Element(2)
-
-  const x = 0
-  const newHead = partition(head, x)
-
-  expect(newHead.value).toBeGreaterThan(x)
-})
-
-test('test without x', () => {
-  const head = new Element(4)
-  head.next = new Element(5)
-  head.next.next = new Element(1)
-  head.next.next.next = new Element(2)
-
-  const x = 3
-  const newHead = partition(head, x)
-
-  expect(newHead.value).toBeLessThan(x)
-  expect(newHead.next.value).toBeLessThan(x)
-})
-
-test('test other values', () => {
-  const head = new Element(1)
-  head.next = new Element(4)
-  head.next.next = new Element(3)
-  head.next.next.next = new Element(2)
-  head.next.next.next.next = new Element(5)
-  head.next.next.next.next = new Element(2)
-
-  const x = 3
-  const newHead = partition(head, x)
-
-  expect(newHead.value).toBeLessThan(x)
-  expect(newHead.next.value).toBeLessThan(x)
-  expect(newHead.next.next.value).toBeLessThan(x)
-})
-
-test('test head greater than x', () => {
-  const head = new Element(6)
-  head.next = new Element(5)
-  head.next.next = new Element(3)
-  head.next.next.next = new Element(4)
-  head.next.next.next.next = new Element(2)
-
-  const x = 3
-  const newHead = partition(head, x)
-
-  expect(newHead.value).toBeLessThan(x)
-})
-
-test('test head less than x', () => {
-  const head = new Element(1)
-  head.next = new Element(5)
-  head.next.next = new Element(3)
-  head.next.next.next = new Element(4)
-  head.next.next.next.next = new Element(2)
-
-  const x = 3
-  const newHead = partition(head, x)
-
-  expect(newHead.value).toBeLessThan(x)
-  expect(newHead.next.value).toBeLessThan(x)
-})
-
-test('test partition in head', () => {
-  const head = new Element(3)
-  head.next = new Element(5)
-  head.next.next = new Element(1)
-  head.next.next.next = new Element(4)
-  head.next.next.next.next = new Element(2)
-
-  const x = 3
-  const newHead = partition(head, x)
-
-  expect(newHead.value).toBeLessThan(x)
-  expect(newHead.next.value).toBeLessThan(x)
-})
-
-test('empty list', () => {
-  const x = 3
-  const newHead = partition(null, x)
-
-  expect(newHead).toBe(null)
+  test('one node', () => {
+    const head = new Element(6)
+    const x = 3
+    const newHead = partitionInOrder(head, x)
+    expect(newHead).toEqual(head)
+  })
 })
